7a1dcc3ba4284d5ad952fdbc08892c3a89df8922
Hernan Menajovsky <hmenajovsky@gmail.com>
2022-01-05 17:57:34 +0100

    Completed lab

--- index.js
+++ index.js
@@ -15,9 +15,49 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then(() => {   
+    // Iteration 2 step to be commented
+    /*Recipe.create(data[0])
+    .then((dbSuccess) => {
+      console.log("The title of the recipe is : ", data[0].title);
+    })
+    .catch((dbError) => {
+      console.log("create recipe failure !");
+    });*/
+    //Iteration 3 step 
+    Recipe.insertMany(data)
+    .then((dbSuccess) => {
+      data.forEach(recipe => console.log("The title of the recipe is : ", recipe.title))      
+    })
+    .catch((dbError) => {
+      console.log("insert many failure !");
+      console.error(dbError);
+    });
+    //Iteration 4 step: needed to add a setTimout to execute the update after the create
+    setTimeout( () =>   {
+      Recipe.findOneAndUpdate(
+      { duration: 220 },
+      { duration: 100 },
+      { new: true })
+      .then((dbSuccess) =>console.log("The recipe has been successfully updated!"))
+      .catch((dbError) => {
+        console.log("update failure !");
+        console.error(dbError);
+      })      
+    }, 1000 )  
+    //Iteration 5 step: needed to add a setTimout to execute the delete after the update and the insert
+    setTimeout( () =>   {
+      Recipe.deleteOne(data[2])
+      .then((dbSuccess) => console.log("The recipe has been successfully deleted!"))        
+      .catch((dbError) => {
+        console.log("insert many failure !");
+        console.error(dbError);
+       })
+    }, 1000)
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+  // iteration 6 : needed to add a setTimeout with a greater delay to execute it after all other tasks
+  setTimeout( (() =>  mongoose.connection.close(() => console.log('Mongoose default connection disconnected '))),2000);
